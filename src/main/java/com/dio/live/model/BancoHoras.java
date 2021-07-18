a273156af484943ea075bb237c570e4b626f3777
Lucas Detogni Pomar <lucaspomar@outlook.com>
2021-07-18 17:25:26 -0300

    Parte 7

--- src/main/java/com/dio/live/model/BancoHoras.java
+++ src/main/java/com/dio/live/model/BancoHoras.java
@@ -2,6 +2,12 @@ package com.dio.live.model;
 
 import lombok.*;
 
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -10,6 +16,20 @@ import lombok.*;
 @Builder
 public class BancoHoras {
 
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class BancoHorasId implements Serializable {
+        private long IdBancoHoras;
+        private long IdMovimentacao;
+        private long IdUsuario;
+    }
 
+    @EmbeddedId
+    private BancoHorasId bancoHorasId;
+    private LocalDateTime dataTrabalhada;
+    private BigDecimal quantidadeHoras;
+    private BigDecimal saldoHoras;
 
 }
