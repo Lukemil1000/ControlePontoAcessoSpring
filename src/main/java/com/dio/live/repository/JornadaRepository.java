3601d732a2046fd03771d992a9e4df7f92f3608d
Lucas Detogni Pomar <lucaspomar@outlook.com>
2021-07-19 15:36:47 -0300

    Parte 10

--- src/main/java/com/dio/live/repository/JornadaRepository.java
+++ src/main/java/com/dio/live/repository/JornadaRepository.java
@@ -1,4 +1,9 @@
 package com.dio.live.repository;
 
-public interface JornadaRepository {
+import com.dio.live.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho,Long> {
 }
